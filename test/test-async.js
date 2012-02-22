d2a8df9b7a2362db98ffca1ff2bdb7106149cd83
zaphod1984 <zaphod84@gmx.de>
2012-02-22 08:58:07 +0100

    added relocking tests

--- test/test-async.js
+++ test/test-async.js
@@ -40,12 +40,118 @@ exports['eventedQueue'] = function( test ){
     }, 50 );
 };
 
-
-exports['eventedQueue queueTriggered'] = function( test ){
+exports['eventedQueue relock'] = function( test ){
 
     var  callOrder      = []
         ,numberCalled   = 0
-        ,eq             = async.eventedQueue( true );
+        ,eq             = async.eventedQueue( false );
+
+    var testFunction = function( nr, time ) {
+        setTimeout( function() {
+            callOrder.push( nr );
+
+            numberCalled++;
+
+            if ( numberCalled == 6 ) {
+                test.same( callOrder, ['task2','task1','task4','task3','task5','task6'] );
+            }
+
+            if ( numberCalled == 7 || numberCalled == 8 ) {
+                test.ok( false, 'should not be could due to relocking' );
+            }
+
+        }, time );
+    };
+
+    eq.push( testFunction, 'task1', 20 );
+    eq.push( testFunction, 'task2', 10 );
+    eq.push( testFunction, 'task3', 40 );
+    eq.push( testFunction, 'task4', 30 );
+
+    test.equal( eq.length(), 4 );
+    test.equal( numberCalled, 0 );
+
+    setTimeout( function() {
+        test.equal( eq.length(), 4 );
+        test.equal( numberCalled, 0 );
+
+        eq.trigger();
+
+        eq.push( testFunction, 'task5', 60 );
+        eq.push( testFunction, 'task6', 80 );
+
+        eq.relock();
+
+        eq.push( testFunction, 'task7', 100 );
+        eq.push( testFunction, 'task8', 120 );
+    }, 50 );
+
+    setTimeout( function() {
+        test.equal( eq.length(), 2 );
+        test.done();
+    }, 200 );
+};
+
+exports['eventedQueue relock retrigger'] = function( test ){
+
+    var  callOrder      = []
+        ,numberCalled   = 0
+        ,eq             = async.eventedQueue( false );
+
+    var testFunction = function( nr, time ) {
+        setTimeout( function() {
+            callOrder.push( nr );
+
+            numberCalled++;
+
+            if ( numberCalled == 6 ) {
+                test.same( callOrder, ['task2','task1','task4','task3','task5','task6'] );
+            }
+
+            if ( numberCalled == 8 ) {
+                test.same( callOrder, ['task2','task1','task4','task3','task5','task6','task7','task8'] );
+                test.done();
+            }
+
+        }, time );
+    };
+
+    eq.push( testFunction, 'task1', 20 );
+    eq.push( testFunction, 'task2', 10 );
+    eq.push( testFunction, 'task3', 40 );
+    eq.push( testFunction, 'task4', 30 );
+
+    test.equal( eq.length(), 4 );
+    test.equal( numberCalled, 0 );
+
+    setTimeout( function() {
+        test.equal( eq.length(), 4 );
+        test.equal( numberCalled, 0 );
+
+        eq.trigger();
+
+        eq.push( testFunction, 'task5', 60 );
+        eq.push( testFunction, 'task6', 80 );
+
+        eq.relock();
+
+        eq.push( testFunction, 'task7', 200 );
+        eq.push( testFunction, 'task8', 220 );
+    }, 50 );
+
+    setTimeout( function() {
+        test.equal( eq.length(), 2 );
+        test.equal( numberCalled, 6 );
+
+        eq.trigger();
+    }, 120 );
+};
+
+exports['eventedQueue autorelock'] = function( test ){
+
+    var  callOrder      = []
+        ,numberCalled   = 0
+        ,eq             = async.eventedQueue( true ); //<--- setting default behaviour to relock after trigger
 
     var testFunction = function( nr, time ) {
         setTimeout( function() {
@@ -1093,7 +1199,7 @@ exports['detectSeries - multiple matches'] = function(test){
 
 exports['sortBy'] = function(test){
     async.sortBy([{a:1},{a:15},{a:6}], function(x, callback){
-        setTimeout(function(){callback(null, x.a);}, 0);
+        setTimeout(function(){callback(null, -1*x.a);}, 0);
     }, function(err, result){
         test.same(result, [{a:1},{a:6},{a:15}]);
         test.done();
